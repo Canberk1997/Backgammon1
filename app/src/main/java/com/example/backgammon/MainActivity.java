c5e57742da00f88401b000451b1434c7e59d410a
jayjoe16 <kilic.caglar@yahoo.de>
2020-05-13 14:15:39 +0200

    caglar abiniz

--- app/src/main/java/com/example/backgammon/MainActivity.java
+++ app/src/main/java/com/example/backgammon/MainActivity.java
@@ -27,7 +27,8 @@ public class MainActivity extends AppCompatActivity {
 private Button btnEinstellungen;
 private Button btnEinzelSpieler;
 private Button btnMehrSpieler;
-private EditText nameInput;
+public String name;
+public EditText nameAmk;
 
 
     @Override
@@ -50,7 +51,10 @@ private EditText nameInput;
         btnEinzelSpieler.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+            nameAmk = (EditText) findViewById(R.id.nameInput);
+            name = nameAmk.getText().toString();
              openActivityEinzelspieler();
+
             }
         });
 
@@ -70,9 +74,13 @@ private EditText nameInput;
 
         public void openActivityEinzelspieler(){
             Intent intent = new Intent(this,ActivityEinzelspieler.class);
-            startActivity(intent);
 
-            writeFB("charlie");
+
+            startActivity(intent);
+            writeFB(name);
+            // writeFB();
+
+
 
         }
 
@@ -84,7 +92,37 @@ private EditText nameInput;
 
         public void writeFB(String name){
         FirebaseDatabase db = FirebaseDatabase.getInstance();
-        db.getReference(name+"/whiteStone/00").setValue(4);
+            for(int i = 0; i<24; i++){
+                if(i == 4){
+                    db.getReference(name+"/blackStone/"+i).setValue(3);
+                }else if(i == 12){
+                    db.getReference(name+"/blackStone/"+i).setValue(5);
+                }else if(i == 7){
+                    db.getReference(name+"/blackStone/"+i).setValue(5);
+                }
+                else if(i == 23){
+                    db.getReference(name+"/blackStone/"+i).setValue(2);
+                } else {
+                    db.getReference(name+"/blackStone/"+i).setValue(0);
+                }
+            }
+            for(int i = 0; i<24; i++){
+
+                if(i == 0){
+                    db.getReference(name+"/whiteStone/"+i).setValue(2);
+                }else if(i == 11){
+                    db.getReference(name+"/whiteStone/"+i).setValue(5);
+                }else if(i == 18){
+                    db.getReference(name+"/whiteStone/"+i).setValue(5);
+                }
+                else if(i == 16){
+                    db.getReference(name+"/whiteStone/"+i).setValue(3);
+                } else {
+                    db.getReference(name+"/whiteStone/"+i).setValue(0);
+                }
+            }
+
+
 
         }
 
@@ -124,43 +162,8 @@ private EditText nameInput;
 
 //alternativ in for schleife
 
-            for(int i = 0; i<24; i++){
 
-                if(i == 4){
-                    database.getReference("Spielfeld/blackStone/"+i).setValue(3);
-                }if(i == ( 7 | 12)){
-                    database.getReference("Spielfeld/blackStone/"+i).setValue(5);
-                }if(i == 23){
-                    database.getReference("Spielfeld/blackStone/"+i).setValue(2);
-                } else {
-                    database.getReference("Spielfeld/blackStone/"+i).setValue(0);
-                }
-            }
 
-            database.getReference("Spielfeld/blackStone/00").setValue(0);
-            database.getReference("Spielfeld/blackStone/11").setValue(0);
-            database.getReference("Spielfeld/blackStone/16").setValue(0);
-            database.getReference("Spielfeld/blackStone/18").setValue(0);
-            database.getReference("Spielfeld/blackStone/01").setValue(0);
-            database.getReference("Spielfeld/blackStone/02").setValue(0);
-            database.getReference("Spielfeld/blackStone/03").setValue(0);
-            database.getReference("Spielfeld/blackStone/04").setValue(3);
-            database.getReference("Spielfeld/blackStone/05").setValue(0);
-            database.getReference("Spielfeld/blackStone/06").setValue(0);
-            database.getReference("Spielfeld/blackStone/07").setValue(5);
-            database.getReference("Spielfeld/blackStone/08").setValue(0);
-            database.getReference("Spielfeld/blackStone/09").setValue(0);
-            database.getReference("Spielfeld/blackStone/10").setValue(0);
-            database.getReference("Spielfeld/blackStone/12").setValue(5);
-            database.getReference("Spielfeld/blackStone/13").setValue(0);
-            database.getReference("Spielfeld/blackStone/14").setValue(0);
-            database.getReference("Spielfeld/blackStone/15").setValue(0);
-            database.getReference("Spielfeld/blackStone/17").setValue(0);
-            database.getReference("Spielfeld/blackStone/19").setValue(0);
-            database.getReference("Spielfeld/blackStone/20").setValue(0);
-            database.getReference("Spielfeld/blackStone/21").setValue(0);
-            database.getReference("Spielfeld/blackStone/22").setValue(0);
-            database.getReference("Spielfeld/blackStone/23").setValue(2);
 
         }
 
